e019160f765f6b6fbcc5ee001205b353195be51a
kelvin Ohemeng <93749415+kelvinohemeng@users.noreply.github.com>
2025-02-05 10:22:06 +0000

    now

--- src/components/react/ReactButton.jsx
+++ src/components/react/ReactButton.jsx
@@ -4,7 +4,7 @@ import Magnet from "../react/reactbits/Magnet";
 
 export const ReactButton = ({ title = "learn more", href = "/" }) => {
   return (
-    <Magnet client:visible padding={20} disabled={false} magnetStrength={10}>
+    <Magnet padding={20} disabled={false} magnetStrength={10}>
       <a href={href}>
         <button class="text-e-lg dark:text-Black-e-black-500 dark:bg-white text-white bg-black full flex gap-3 items-center px-6 py-4 rounded-full">
           <span>{title}</span>
